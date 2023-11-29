1a96a62fb008b41b7aa7bc2d71a9449887215f8a
Łukasz Olbrycht <lukasz.olbrycht@gmail.com>
2023-11-29 13:17:00 +0100

    Add for nad query selectior

--- js/script.js
+++ js/script.js
@@ -1,1 +1,30 @@
-'use strict'
+'use strict';
+{
+    /* document.getElementById('test-button').addEventListener('click', function(){
+    const links = document.querySelectorAll('.titles a');
+    console.log('links:', links)
+}) */
+    const titleClickHandler = function(event){
+        console.log('Link was clicked');
+        console.log(event);
+        /* remove class 'active' from all article links  */
+
+        /* add class 'active' to the clicked link */
+        this.classList.add('active');
+
+        /* remove class 'active' from all articles */
+
+        /* get 'href' attribute from the clicked link */
+
+        /* find the correct article using the selector (value of 'href' attribute) */
+
+        /* add class 'active' to the correct article */
+    }
+    const links = document.querySelectorAll('.titles a')
+
+    for (const link of links){
+         link.addEventListener('click', titleClickHandler);
+    }
+
+}
+
